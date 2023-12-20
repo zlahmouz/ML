5c6510b0d2c5efe5cd53b65973e148e1eaf4b884
zlahmouz <104124709+zlahmouz@users.noreply.github.com>
2023-12-20 01:01:05 +0100

    Delete Desktop/Courses directory

--- Desktop/Courses/ENM_N7_ML/Readme.txt
+++
@@ -1,2 +0,0 @@
-I would like to express my gratitude to Rachid Elmontassir and Pierre Lepetit for guiding this project and providing valuable insights.
-
--- Desktop/Courses/Parallel_Calculus/BE2022/02_normA/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# à modifier en indiquant où est installé simgrid
-SIMGRID=/mnt/n7fs/ens/tp_guivarch/opt2023/simgrid-3.32
-
-export PATH=${SIMGRID}/bin:${PATH}
-
-alias smpirun="smpirun -hostfile ${PWD}/archis/cluster_hostfile.txt -platform ${PWD}/archis/cluster_crossbar.xml"
